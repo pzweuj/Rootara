801a67cdc37fa25130a90e98cc46a14d8dc90d62
EuJ <pzweuj@live.com>
2025-04-27 23:19:43 +0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Dna, AlertCircle, Globe, Moon, Sun } from "lucide-react"
+import { Dna, AlertCircle, Moon, Sun } from "lucide-react"
 import { Alert, AlertDescription } from "@/components/ui/alert"
 import { useTheme } from "next-themes"
 
