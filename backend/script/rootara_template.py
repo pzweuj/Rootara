eff3669db54126c7a613d2c522650cf03bf247a3
EuJ <pzweuj@live.com>
2025-05-07 17:35:02 +0800

    Delete rootara_template.py

--- backend/script/rootara_template.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-# 模板表格的建立
-
-from rootara_reader import csv_create
-
-
-csv_create('../Test/pzw_wegene.txt', '../Test/pzw_wegene.rootara.txt', method='wegene', rootara_core='../Rootara.core.202404.txt.gz')
-
